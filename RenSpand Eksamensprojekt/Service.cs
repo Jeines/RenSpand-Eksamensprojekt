6ebc2c5f904f644fff141232d6337bd0626601bc
Rasmus Emil Olsen <rao003@edu.zealand.dk>
2025-04-28 12:24:10 +0200

    Lavet Service class og ServiceItem Class. m. List

---
+++ RenSpand Eksamensprojekt/Service.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenSpand_Eksamensprojekt
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Service(int id, string name, string description, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+        public Service() { }
+    }
+}
+
---
+++ RenSpand Eksamensprojekt/ServiceItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenSpand_Eksamensprojekt
+{
+    public class ServiceItem
+    {
+        private int Amount { get; set; }
+
+
+        public ServiceItem(int amount)
+        {
+            Amount = amount;
+        }
+
+        public ServiceItem() { }
+    }
+    public class ServiceItemList
+    {
+        public List<ServiceItem> ServiceItems { get; set; }
+        public ServiceItemList()
+        {
+            ServiceItems = new List<ServiceItem>();
+        }
+    }
+}
