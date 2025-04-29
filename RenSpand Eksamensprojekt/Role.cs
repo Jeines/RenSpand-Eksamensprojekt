4371eb9aa35724462f17362e8a9d7fea5fb3dff5
Rasmus Emil Olsen <rao003@edu.zealand.dk>
2025-04-29 10:53:03 +0200

    Enum med roller

---
+++ RenSpand Eksamensprojekt/Role.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenSpand_Eksamensprojekt
+{
+    public enum Role
+    {
+        [Description("Administrator")]
+        Admin,
+        
+        [Description("Employee")]
+        Employee,
+        
+        [Description("Business")]
+        Business,
+        
+        [Description("Private")]
+        Private,
+        
+        [Description("Guest")]
+        Guest
+
+    }
+}
