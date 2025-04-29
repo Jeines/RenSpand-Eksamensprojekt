768af68d555128bd34c8c3a4db9f5e8aafc8c2f2
Mikkel Kornerup Andersen <Mkkmkm75@gmail.com>
2025-04-29 10:56:17 +0200

    Profile & Employee er blevet lavet og skal nu merges in i main

---
+++ RenSpand Eksamensprojekt/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenSpand_Eksamensprojekt
+{
+    public class Employee
+    {
+       public int YearsOfExperians { get; set; }
+       
+        public decimal Salary { get; set; }
+
+        public List<string> Qualifications { get; set; } = new List<string>();
+
+        public Employee(int yearsOfExperians, decimal salary, List<string> qualifications)
+        {
+            YearsOfExperians = yearsOfExperians;
+            Salary = salary;
+            Qualifications = qualifications;
+        }
+
+        public Employee() { }
+
+    }
+}
