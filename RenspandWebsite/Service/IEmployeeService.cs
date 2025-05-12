4d5efd3564ca1a3bb72852fc08f513530c31c456
Jens Hincke Friis <jef003@edu.zealand.dk>
2025-05-12 11:36:18 +0200

    sat IemployeeService færdig

--- RenspandWebsite/Service/IEmployeeService.cs
+++ RenspandWebsite/Service/IEmployeeService.cs
@@ -1,6 +1,14 @@
-﻿namespace RenspandWebsite.Service
+﻿using RenSpand_Eksamensprojekt;
+
+namespace RenspandWebsite.Service
 {
     public interface IEmployeeService
     {
+        List<Employee> GetEmployees();
+        void AddEmployee(Employee employee);
+        void UpdateEmployee(Employee employee);
+        Employee GetEmployee(int id);
+        Employee DeleteEmployee(int? employeeId);
+
     }
 }
