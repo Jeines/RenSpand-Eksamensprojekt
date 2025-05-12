b029476262a05789ed3897266ffff6639d6fc4f1
Jens Hincke Friis <jef003@edu.zealand.dk>
2025-05-12 11:43:05 +0200

    .cs Delete emplyee færdig har RenSpand_Eksamensprojekt.Employee i

--- RenspandWebsite/Pages/Admin/AdminEmployee/DeleteEmployee.cshtml.cs
+++ RenspandWebsite/Pages/Admin/AdminEmployee/DeleteEmployee.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RenSpand_Eksamensprojekt;
+using RenspandWebsite.Service;
 
 namespace RenspandWebsite.Pages.Admin.AdminEmployee
 {
     public class DeleteEmployeeModel : PageModel
     {
-        public void OnGet()
+        private IEmployeeService _employeeService;
+        public DeleteEmployeeModel(IEmployeeService employeeService)
         {
+            _employeeService = employeeService;
+        }
+        [BindProperty]
+        public RenSpand_Eksamensprojekt.Employee Employee { get; set; }
+        public IActionResult OnGet(int id)
+        {
+            Employee = _employeeService.GetEmployee(id);
+            if (Employee == null)
+                return RedirectToPage("/NotFound"); //NotFound er ikke defineret endnu
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            RenSpand_Eksamensprojekt.Employee deletedEmployee = _employeeService.DeleteEmployee(Employee.Id);
+            if (deletedEmployee == null)
+                return RedirectToPage("/NotFound"); //NotFound er ikke defineret endnu
+            return RedirectToPage("GetAllEmployees");
         }
     }
 }
