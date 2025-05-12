46e3f46ed79e811045e4974dd753f3730b8bfceb
Jens Hincke Friis <jef003@edu.zealand.dk>
2025-05-12 11:29:00 +0200

    Mappe plus razor pages oprettet

---
+++ RenspandWebsite/Pages/Admin/AdminEmployee/EditEmployee.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RenspandWebsite.Pages.Admin.AdminEmployee
+{
+    public class EditEmployeeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
