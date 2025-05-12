c79d17c9b8f319500383a86995dde663976f0932
Jens Hincke Friis <jef003@edu.zealand.dk>
2025-05-12 12:03:25 +0200

    .cs edit

--- RenspandWebsite/Pages/Admin/AdminProduct/EditProduct.cshtml.cs
+++ RenspandWebsite/Pages/Admin/AdminProduct/EditProduct.cshtml.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RenSpand_Eksamensprojekt;
+using RenspandWebsite.Service;
 
 namespace RenspandWebsite.Pages.Admin.AdminProduct
 {
     public class EditProductModel : PageModel
     {
-        public void OnGet()
+        private IWorkService _productService;
+
+        public EditProductModel(IWorkService productService)
         {
+            _productService = productService;
+        }
+
+        [BindProperty]
+        public Work Product { get; set; }
+        public IActionResult OnGet(int id)
+        {
+            Product = _productService.GetWork(id);
+            if (Product == null)
+                return RedirectToPage("/NotFound"); //NotFound er ikke defineret endnu
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _productService.UpdateWork(Product);
+            return RedirectToPage("GetAllProducts");
         }
     }
 }
