34dc05a6e157fa5b30a94ee44f5736982df657bb
Jens Hincke Friis <jef003@edu.zealand.dk>
2025-05-12 11:58:59 +0200

    add mappe og razorpages

---
+++ RenspandWebsite/Pages/Admin/AdminProduct/CreateProduct.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RenspandWebsite.Pages.Admin.AdminProduct
+{
+    public class CreateProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RenspandWebsite/Pages/Admin/AdminProduct/DeleteProduct.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RenspandWebsite.Pages.Admin.AdminProduct
+{
+    public class DeleteProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
