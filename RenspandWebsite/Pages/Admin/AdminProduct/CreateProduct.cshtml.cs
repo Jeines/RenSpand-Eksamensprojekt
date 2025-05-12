566c5364d75fc8ceb4249c91e06b267dff20ab4f
Jens Hincke Friis <jef003@edu.zealand.dk>
2025-05-12 12:00:45 +0200

    .cs create product

--- RenspandWebsite/Pages/Admin/AdminProduct/CreateProduct.cshtml.cs
+++ RenspandWebsite/Pages/Admin/AdminProduct/CreateProduct.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RenSpand_Eksamensprojekt;
+using RenspandWebsite.Service;
+using System.Reflection;
 
 namespace RenspandWebsite.Pages.Admin.AdminProduct
 {
     public class CreateProductModel : PageModel
     {
-        public void OnGet()
+        private IWorkService _productService;
+        public CreateProductModel(IWorkService productService)
         {
+            _productService = productService;
+        }
+        [BindProperty]
+        public Work Product { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _productService.AddWork(Product);
+            return RedirectToPage("GetAllProducts");
         }
     }
 }
