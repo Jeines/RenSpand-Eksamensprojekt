a9269ae0cf5ebd93e6209ea78998040d3a2669ad
Jens Hincke Friis <jef003@edu.zealand.dk>
2025-05-13 16:29:20 +0200

    EmployeePage ændret så tingene gemme i  db og så den bruger den nu rigtige Service

--- RenspandWebsite/Pages/Employee/EmployeePage.cshtml.cs
+++ RenspandWebsite/Pages/Employee/EmployeePage.cshtml.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RenSpand_Eksamensprojekt;
 using RenspandWebsite.Service;
-using RenspandWebsite.Service.OrderServices;
 
 namespace RenspandWebsite.Pages.Employee
 {
@@ -12,50 +11,88 @@ namespace RenspandWebsite.Pages.Employee
 
         private readonly OrderService _orderService;
 
-        /// <summary>
-        /// loads all orders when page is loaded with a Get request
-        /// </summary>
-        public void OnGet()
-        {
-            Orders = _orderService.GetOrders().ToList();
-            foreach (var order in Orders)
-            {
-                Console.WriteLine("Order ID: " + order.Id);
-                Console.WriteLine("Order Status: " + order.AcceptStatus);
-
-            }
-        }
-
         public EmployeePageModel(OrderService orderServices)
         {
             _orderService = orderServices;
         }
 
+        /// <summary>
+        /// Asynchront metode til at hente alle ordrer og gemme dem i Orders-listen.
+        /// </summary>
+        /// <returns></returns>
+        public async Task OnGetAsync()
+        {
+            Orders = await _orderService.GetAllOrdersAsync();
 
-        //TODO: FIX AcceptOrder and RejectOrder and update in the database
+            // Uncomment the following lines to see the order details in the console
+            //foreach (var order in Orders)
+            //{
+            //    Console.WriteLine("Order ID: " + order.Id);
+            //    Console.WriteLine("Order Status: " + order.AcceptStatus);
+            //}
+        }
+
+        /// <summary>
+        /// Accepts order når et given Id er sendt som parameter og genindlæser siden.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> OnPostAcceptOrderAsync(int id)
+        {
+            //Console.WriteLine("AcceptOrder - ID: " + orderId);
+            await _orderService.AcceptOrderAsync(id);
+            return RedirectToPage();
+        }
+
+        /// <summary>
+        /// Rejects order når et givet Id er sendt som parameter og genindlæser siden.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> OnPostRejectOrderAsync(int id)
+        {
+            Console.WriteLine("RejectOrder - ID: " + id);
+            await _orderService.RejectOrderAsync(id);
+            return RedirectToPage();
+        }
+
+        /// <summary>
+        /// Gemmer en note for ordren med det givne id og genindlæser siden.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="note"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> OnPostSaveNoteAsync(int id, string note)
+        {
+            Console.WriteLine("SaveNote - ID: " + id);
+            await _orderService.SaveNoteAsync(id, note);
+            return RedirectToPage();
+        }
+
+        //TODO: Fjernes hvis OnPost metoderne virker
         ///// <summary>
         ///// Accepts order with the given orderId and reloads page
         ///// </summary>
         ///// <param name="orderId"></param>
         ///// <returns></returns>
-        public IActionResult OnPostAcceptOrder(int orderId)
-        {
-            Console.WriteLine("Acceptorder - ID: " + orderId);
-            _orderService.AcceptOrder(orderId);
-            return RedirectToPage();
-        }
+        //public IActionResult OnPostAcceptOrder(int orderId)
+        //{
+        //    Console.WriteLine("Acceptorder - ID: " + orderId);
+        //    _orderService.AcceptOrder(orderId);
+        //    return RedirectToPage();
+        //}
 
         /// <summary>
         /// Rejects order with the given orderId and reloads page
         /// </summary>
         /// <param name="orderId"></param>
         /// <returns></returns>
-        public IActionResult OnPostRejectOrder(int orderId)
-        {
-            Console.WriteLine("RejectOrder");
-            _orderService.RejectOrder(orderId);
-            return RedirectToPage();
-        }
+        //public IActionResult OnPostRejectOrder(int orderId)
+        //{
+        //    Console.WriteLine("RejectOrder");
+        //    _orderService.RejectOrder(orderId);
+        //    return RedirectToPage();
+        //}
 
         /// <summary>
         /// Saves a note for the order with the given orderId and reloads page
@@ -63,11 +100,11 @@ namespace RenspandWebsite.Pages.Employee
         /// <param name="orderId"></param>
         /// <param name="note"></param>
         /// <returns></returns>
-        public IActionResult OnPostSaveNote(int orderId, string note)
-        {
-            Console.WriteLine("SaveNote");
-            _orderService.SaveNote(orderId, note);
-            return RedirectToPage();
-        }
+        //public IActionResult OnPostSaveNote(int orderId, string note)
+        //{
+        //    Console.WriteLine("SaveNote");
+        //    _orderService.SaveNote(orderId, note);
+        //    return RedirectToPage();
+        //}
     }
 }
