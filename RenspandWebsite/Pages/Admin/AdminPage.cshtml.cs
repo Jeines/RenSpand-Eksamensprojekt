267925d1f2db6edc186b2c0a25ec79141e873bc5
Thomas Larsen <Larsen.thomas.dk@gmai.com>
2025-05-14 12:39:35 +0200

    assa<D

--- RenspandWebsite/Pages/Admin/AdminPage.cshtml.cs
+++ RenspandWebsite/Pages/Admin/AdminPage.cshtml.cs
@@ -17,22 +17,7 @@ namespace RenspandWebsite.Pages.Admin
         //TODO: Remove onget async and post async methods when done testing
         public async Task OnGetAsync()
         {
-            //Console.WriteLine("\nAdding test profile to DB...");
-            //await _profileService.AddTestAddressAsync();
-
             var profiles = _profileService.Profiles; 
-            foreach (var profile in profiles)
-            {
-                Console.WriteLine($"Id: {profile.Id}, Username: {profile.Username}, Email: {profile.Email}");
-            }
-
-
-            //Console.WriteLine("\nFrom Database:");
-            //var dbProfiles = await _profileService.GetProfilesFromDb();
-            //foreach (var profile in dbProfiles)
-            //{
-            //    Console.WriteLine(profile.ToString());
-            //}
         }
     }
 }
