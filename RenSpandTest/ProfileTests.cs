869fe228ebb0b1536c6431169cca53cfdd05ac92
Jens Hincke Friis <jef003@edu.zealand.dk>
2025-06-10 00:40:25 +0200

    Sat kommentar på unit testen

--- RenSpandTest/ProfileTests.cs
+++ RenSpandTest/ProfileTests.cs
@@ -9,17 +9,26 @@ using RenspandWebsite.Service.UnitTestClasses;
 
 namespace RenSpandTest
 {
+    /// <summary>
+    /// Testklasse for at oprette profiler i RenSpand-appen.
+    /// </summary>
     [TestClass]
     public class CreateModelTests
     {
+        // Servicen for at oprette profiler
         private UnitTestProfileCreate _profileService;
 
+        // Test initialisering, der køres før hver test
         [TestInitialize]
         public void Setup()
         {
+            // Initialiserer en ny instans af UnitTestProfileCreate for hver test methode
             _profileService = new UnitTestProfileCreate();
         }
 
+        /// <summary>
+        /// tester en gyldig profiloprettelse.
+        /// </summary>
         [TestMethod]
         public void ValidInput()
         {
@@ -32,14 +41,19 @@ namespace RenSpandTest
             };
 
             // Act
+            // Tilføjer profilen til service
             _profileService.AddProfile(profile);
 
             // Assert
+            // Kontrollerer at profilen er blevet tilføjet korrekt
             Assert.AreEqual(1, _profileService.Profiles.Count);
             Assert.AreEqual("validUser", _profileService.Profiles[0].Username);
             Assert.AreEqual("valid@example.com", _profileService.Profiles[0].Email);
         }
 
+        /// <summary>
+        /// Tester en ugyldig brugernavn, der er for kort.
+        /// </summary>
         [TestMethod]
         public void InvalidUsername()
         {
@@ -55,6 +69,9 @@ namespace RenSpandTest
             Assert.ThrowsException<InvalidUsernameException>(() => _profileService.AddProfile(profile));
         }
 
+        /// <summary>
+        /// Tester en ugyldig adgangskode, der er for kort.
+        /// </summary>
         [TestMethod]
         public void InvalidPassword()
         {
@@ -69,7 +86,9 @@ namespace RenSpandTest
             Assert.ThrowsException<InvalidPasswordException>(() => _profileService.AddProfile(profile));
         }
 
-
+        /// <summary>
+        /// Tester en ugyldig e-mail, der ikke er i korrekt format.
+        /// </summary>
         [TestMethod]
         public void InvalidEmail()
         {
